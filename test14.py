1363beb9c7d94a50ee66bd44eb3854c858a596f1
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-03-28 12:37:15 -0700

    Add files via upload

---
+++ test14.py
@@ -0,0 +1,37 @@
+import sqlite3
+fileList = [('information.docx'),
+            ('Hello.txt'),
+            ('myImage.png'),
+            ('myMovie.mpg'),
+            ('World.txt'),
+            ('data.pdf'),
+            ('myPhoto.jpg')]
+
+conn = sqlite3.connect('cool.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_files( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        file_name TEXT \
+        )")
+    conn.commit()
+conn.close
+()
+
+with conn:
+    cur = conn.cursor()
+    for item in fileList:
+        if item.endswith(".txt"):
+            cur.execute("INSERT INTO tbl_files(file_name) VALUES(?)", (item,))
+            conn.commit()
+conn.close()
+
+conn = sqlite3.connect('cool.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tbl_files ")
+    varPerson = cur.fetchall()
+    print(varPerson)
+conn.close()
